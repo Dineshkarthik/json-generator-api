ca0b0285858b953101af8f4e95a3c06ec841a694
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-11-06 17:47:42 +0530

    ENH: Converting to Flask App

--- json_generator.py
+++ json_generator.py
@@ -1,7 +1,13 @@
 """Script to generate json."""
+import os
 import json
+import hashlib
 from faker import Faker
+from optparse import OptionParser
+from flask import Flask, request, redirect, url_for, render_template
 
+app = Flask(__name__)
+app.secret_key = hashlib.sha1(os.urandom(128)).hexdigest()
 fake = Faker()
 
 
@@ -21,3 +27,15 @@ def get_json(json_schema):
     for key, value in json_schema.items():
         dict_[key] = generate_value(value)
     return json.dumps(dict_)
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option(
+        "-p",
+        "--port",
+        dest="port",
+        help="Port on which the app will run",
+        default=5000)
+    (options, args) = parser.parse_args()
+    app.run(host='0.0.0.0', debug=True, port=int(options.port))
