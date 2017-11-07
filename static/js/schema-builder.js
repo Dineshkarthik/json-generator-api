1d8f34b61d1ad842ca1c3342a9874f61cc2d1ae4
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-11-07 17:06:55 +0530

    ADD: Schema Builder Js

---
+++ static/js/schema-builder.js
@@ -0,0 +1,42 @@
+var rules_basic = {
+    rules: [{
+        id: 'name',
+        operator: 'equal',
+        value: "user_name"
+    }]
+};
+
+$('#schema-builder').queryBuilder({
+    plugins: ['bt-tooltip-errors'],
+
+    filters: [{
+        id: 'name',
+        label: 'Full Name',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'first_name',
+        label: 'First Name',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'last_name',
+        label: 'Last Name',
+        type: 'string',
+        operators: ['equal']
+    }],
+
+    rules: rules_basic
+});
+
+$('#btn-reset').on('click', function() {
+    $('#schema-builder').queryBuilder('reset');
+});
+
+$('#btn-submit').on('click', function() {
+    var result = $('#schema-builder').queryBuilder('getRules');
+
+    if (!$.isEmptyObject(result)) {
+        console.log(JSON.stringify(result, null, 2));
+    }
+});
