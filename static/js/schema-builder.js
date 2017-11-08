4b978aea7bffa57e5f30843308bd0987146d33c9
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-11-08 16:38:13 +0530

    ENH: More Generators

--- static/js/schema-builder.js
+++ static/js/schema-builder.js
@@ -1,8 +1,29 @@
 var rules_basic = {
+    condition: 'AND',
     rules: [{
-        id: 'name',
+        id: 'first_name',
         operator: 'equal',
-        value: "user_name"
+        value: "user_first_name"
+    },{
+        id: 'last_name',
+        operator: 'equal',
+        value: "user_last_name"
+    },{
+        id: 'ascii_email',
+        operator: 'equal',
+        value: "email"
+    },{
+        id: 'phone_number',
+        operator: 'equal',
+        value: "telephone_number"
+    },{
+        id: 'address',
+        operator: 'equal',
+        value: "address"
+    },{
+        id: 'image_url',
+        operator: 'equal',
+        value: "image"
     }]
 };
 
@@ -24,8 +45,117 @@ $('#schema-builder').queryBuilder({
         label: 'Last Name',
         type: 'string',
         operators: ['equal']
+    },{
+        id: 'word',
+        label: 'String',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'address',
+        label: 'Address',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'ascii_email',
+        label: 'Email',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'boolean',
+        label: 'Boolean',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'chrome',
+        label: 'Chrome User-Agent',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'city',
+        label: 'City',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'color_name',
+        label: 'Color',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'country',
+        label: 'Country',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'country_code',
+        label: 'Country Code',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'date',
+        label: 'Date',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'iso8601',
+        label: 'Datetime',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'file_extension',
+        label: 'File Extension',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'file_name',
+        label: 'File Name',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'hex_color',
+        label: 'Hex Color Code',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'image_url',
+        label: 'Image Url',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'ipv4',
+        label: 'IP V4',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'mime_type',
+        label: 'Mime-Type',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'month_name',
+        label: 'Month Name',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'phone_number',
+        label: 'Phone Number',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'text',
+        label: 'Text',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'timezone',
+        label: 'Time Zone',
+        type: 'string',
+        operators: ['equal']
+    }, {
+        id: 'tld',
+        label: 'Top Level Domain',
+        type: 'string',
+        operators: ['equal']
     }],
-
     rules: rules_basic
 });
 
